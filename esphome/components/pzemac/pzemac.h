ce931887dfcffc32619d7a1b5bd5003c7215ceda
garry0garry <95581374+garry0garry@users.noreply.github.com>
2025-11-01 21:51:40 +0300

    Update pzemac.h
    pre_ to My_

--- esphome/components/pzemac/pzemac.h
+++ esphome/components/pzemac/pzemac.h
@@ -38,8 +38,8 @@ class PZEMAC : public PollingComponent, public modbus::ModbusDevice {
 
   void reset_energy_();
 
-  uint32_t pre_last_update_time_;
-  float pre_last_energy_sensor;
+  uint32_t My_last_update_time_;
+  float My_last_energy_sensor;
 };
 
 template<typename... Ts> class ResetEnergyAction : public Action<Ts...> {
